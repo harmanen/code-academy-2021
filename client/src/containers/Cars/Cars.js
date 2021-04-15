34468826c4e934dbf02820c71c354ea932b50b4d
Jussi Harmanen <hurmanen@gmail.com>
2021-04-15 11:56:51 +0300

    Implement cars data fetching

--- client/src/containers/Cars/Cars.js
+++ client/src/containers/Cars/Cars.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row } from "reactstrap";
 import Car from "./Car";
 import "./Cars.css";
 
 export default function Cars(props) {
-  const cars = [
-    { id: 1, make: "ford", model: "Mustang" },
-    { id: 2, make: "honda", model: "Civic" },
-    { id: 3, make: "skoda", model: "Octavia" },
-    { id: 4, make: "honda", model: "Accord" },
-    { id: 5, make: "skoda", model: "Fabia" },
-    { id: 6, make: "ford", model: "T" },
-    { id: 7, make: "volvo", model: "V40" },
-  ];
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    fetch("api/cars")
+      .then((res) => res.json())
+      .then((data) => setCars(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Row id="cars">
       {cars.map((car) => (
