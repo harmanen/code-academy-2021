ab34b0971364de760fe1a726848ebbc0a5d8974e
Jussi Harmanen <hurmanen@gmail.com>
2021-05-28 11:30:50 +0300

    Add CarsController

---
+++ jsbackend/cars/CarsController.js
@@ -0,0 +1,35 @@
+const cars = [
+  { id: 1, make: "ford", model: "Mustang" },
+  { id: 2, make: "honda", model: "Civic" },
+];
+
+export default (app) => {
+  app.get("/Cars", (req, res) => {
+    res.json(cars);
+  });
+
+  app.post("/Cars", (req, res) => {
+    const { body } = req;
+    const { make, name } = body;
+
+    console.log(make, name);
+
+    const id = cars.reduce((maxId, car) => Math.max(maxId, car.id), 0);
+    const newCar = { id: id + 1, make, name };
+
+    cars.push(newCar);
+    res.json(newCar);
+  });
+
+  app.delete("/Cars/:id", (req, res) => {
+    const { params } = req;
+
+    console.log(params);
+
+    const n = cars.findIndex((car) => car.id === parseInt(params.id));
+
+    if (n > -1) {
+      cars.splice(n, 1);
+    }
+  });
+};
