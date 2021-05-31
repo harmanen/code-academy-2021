d38908d4780d0a6585a596e5660c652f23a40f92
Jussi Harmanen <hurmanen@gmail.com>
2021-05-31 15:25:21 +0300

    Add cars to graphql

--- jsbackend/cars/CarsRepository.js
+++ jsbackend/cars/CarsRepository.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
 import CarsSchema from "./CarsSchema.js";
+import { composeWithMongoose } from "graphql-compose-mongoose";
 
 export const CarModel = mongoose.model("Car", CarsSchema);
+export const CarTC = composeWithMongoose(CarModel);
