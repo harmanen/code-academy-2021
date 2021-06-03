69d210f5cba77b4a1fa1449be36b7a215d7bd66b
Jussi Harmanen <hurmanen@gmail.com>
2021-06-03 15:06:26 +0300

    Add Dockerfile and .dockerignore

--- jsbackend/index.js
+++ jsbackend/index.js
@@ -10,15 +10,15 @@ const VERY_SECRET_KEY = "akjngfwoeinoi2n3n2rlk3nlknslf";
 app.use(cookieParser(VERY_SECRET_KEY));
 app.use(bodyParser.json());
 
-await mongoose.connect(
-  "mongodb://root:example@localhost/code_academy?authSource=admin",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  }
-);
+// await mongoose.connect(
+//   "mongodb://root:example@localhost/code_academy?authSource=admin",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//   }
+// );
 
 import AuhtController from "./AuthController.js";
 import GraphQlController from "./GraphQlController.js";
