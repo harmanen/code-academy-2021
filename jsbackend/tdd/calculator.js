2b41f96c757095896d5096bba85a319d52a5da90
Jussi Harmanen <hurmanen@gmail.com>
2021-06-07 15:34:15 +0300

    Implement functions based on tests

--- jsbackend/tdd/calculator.js
+++ jsbackend/tdd/calculator.js
@@ -1,3 +1,21 @@
-export const convertStringToNumber = (string) => {};
-export const convertNumberToString = (number) => {};
-export const sum = (a, b) => {};
+const mapStringsToNumbers = {
+  one: 1,
+  two: 2,
+  five: 5,
+  seven: 7,
+  nine: 9,
+  ten: 10,
+};
+
+const mapNumbersToStrings = Object.entries(mapStringsToNumbers).reduce(
+  (acc, [key, value]) => ({ ...acc, [value]: key }),
+  {}
+);
+
+export const convertStringToNumber = (string) =>
+  parseInt(mapStringsToNumbers[string.toLowerCase()]);
+
+export const convertNumberToString = (number) => mapNumbersToStrings[number];
+
+export const sum = (a, b) =>
+  mapNumbersToStrings[mapStringsToNumbers[a] + mapStringsToNumbers[b]];
